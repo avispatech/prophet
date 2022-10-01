f9eaa8e9b5d407926e50f245b9f835e16481546a
Leonardo Luarte G <leonardo@luarte.net>
2022-09-30 21:08:29 -0300

    Adds some tests

--- leo.rb
+++ leo.rb
@@ -65,35 +65,31 @@ STATIONS = station_hash
 
 class Planner
 
-  def travel(from:, to:, direction: 0, visited_lines: [], station_hash: STATIONS, db: {})
+  def initialize(db: {})
+    @db = db
+  end
+
+  def travel(from:, to:, direction: 0, visited_lines: [], station_hash: STATIONS, depth: 0)
     cache_key = "#{from}->#{to}"
     station = station_hash[from]
     destination_station = station_hash[to]
-    # puts "travel from:#{from} (#{station.name}) to:#{to} direction:#{direction} visited_lines: #{visited_lines}"
+
     visited_lines << station.line unless visited_lines.include? station.line
     visited_lines = visited_lines.dup
-    if from == to
-      # puts "Im at the last station!"
-      return Hashie::Mash.new({ stations: [station], cost: 1})
-    end
-    # if db.key? cache_key
-    #   return db[cache_key]
-    # end
+    return Hashie::Mash.new({ stations: [station], cost: 1}) if from == to
 
     results = []
     unless destination_station.line == station.line
       if station.connecting
         comb_station = station.connecting_station
         if comb_station.id == to
-          # puts "Combination station is the last station!"
           return Hashie::Mash.new({ stations: [station], cost: 1 })
         end
         unless visited_lines.include? comb_station.line
           visited_lines << station.line
-          # puts "\t\t travel #{comb_station.id} -> #{to} (1)"
-          results << travel(from: comb_station.id, to:, direction: 1, visited_lines: visited_lines.dup, db:)
-          # puts "\t\t travel #{comb_station.id} -> #{to} (-1)"
-          results << travel(from: comb_station.id, to:, direction: -1, visited_lines: visited_lines.dup, db:)
+
+          results << travel(from: comb_station.id, to:, direction: 1, visited_lines: visited_lines.dup, depth: depth + 1)
+          results << travel(from: comb_station.id, to:, direction: -1, visited_lines: visited_lines.dup, depth: depth + 1)
         end
       end
     end
@@ -101,12 +97,10 @@ class Planner
     next_station = station_hash[(from.to_i + 1).to_s]
     prev_station = station_hash[(from.to_i - 1).to_s]
     if direction != 1 and prev_station
-      # puts "\t\t travel #{prev_station.id} -> #{to} (-1)"
-      results << travel(from: prev_station.id.to_s, to:, direction: -1, visited_lines: visited_lines.dup, db:)
+      results << travel(from: prev_station.id.to_s, to:, direction: -1, visited_lines: visited_lines.dup, depth: depth + 1)
     end
     if direction != -1 and next_station
-      # puts "\t\t travel #{next_station.id} -> #{to} (1)"
-      results << travel(from: next_station.id.to_s, to:, direction: 1, visited_lines: visited_lines.dup, db:)
+      results << travel(from: next_station.id.to_s, to:, direction: 1, visited_lines: visited_lines.dup, depth: depth + 1)
     end
     
     # This is a dead end
@@ -119,18 +113,18 @@ class Planner
     best_result.stations << station
     best_result.cost += 1
 
-    # db[cache_key] = best_result
+    # @result_db[cache_key] = best_result
     # puts "Will return(FINAL) #{best_result}"
     best_result
   rescue StandardError => e
     puts "Error: #{e.inspect}"
   end
 end
-
-CROPPED_STATIONS = STATIONS.keys.select { |k|  k.to_i % 11 == 0 }
+random = Random.new(100)
+CROPPED_STATIONS = STATIONS.keys.select { |k|  random.rand > 0.96 }
 CROP_STATION_ZIP = []
 CROPPED_STATIONS.each { |o| CROPPED_STATIONS.each { |d| CROP_STATION_ZIP << [o, d] } }
-puts "#{CROP_STATION_ZIP.length} stations loaded for test"
+puts "#{CROP_STATION_ZIP.length} stations tuples loaded for test"
 
 def calculate_matrix_parallel_threads
   tuples = CROP_STATION_ZIP
@@ -152,10 +146,9 @@ end
 
 def calculate_matrix
   distance_matrix = {}
-
   CROP_STATION_ZIP.each do |from, to|
-    
-    result = Planner.new.travel(from:, to:)
+    planner = Planner.new(db: distance_matrix)
+    result = planner.travel(from:, to:)
     # puts "#{from} -> #{to} = #{result.cost}"
     distance_matrix["#{from}->#{to}"] = result
   end
@@ -167,10 +160,18 @@ def calculate_matrix
 end
 
 def calculate_matrices
+  res = []
   Benchmark.bm do |benchmark|
-    benchmark.report('processes') { calculate_matrix_parallel_processes }
-    benchmark.report('threads') { calculate_matrix_parallel_threads }
-    benchmark.report('inline') { calculate_matrix }
+    benchmark.report('processes') { res << calculate_matrix_parallel_processes }
+    # benchmark.report('threads') { res << calculate_matrix_parallel_threads }
+    benchmark.report('inline') { res << calculate_matrix }
+  end
+  ARGF.readline 
+  res.each do |matrix|
+    matrix.each do |route, result|
+      puts "#{route}: #{result.stations.reverse.map(&:id).join(',')}"
+    end
+    ARGF.readline    
   end
 end
 
@@ -186,26 +187,24 @@ def calculate_route(station_names:)
   puts station_names[destination]
   raise if station_names[destination].nil?
 
-  Benchmark.bm do |benchmark|
-    benchmark.report('one') do
-      res = Planner.new.travel(from: origin.to_s, to: destination.to_s)
-      puts res.stations.reverse.map { |s| "#{s.id} #{s.name} (#{s.line})"}.join("\n")
-    end
-  end
+
+  res = Planner.new.travel(from: origin.to_s, to: destination.to_s)
+  puts res.stations.reverse.map { |s| "#{s.id} #{s.name} (#{s.line})"}.join("\n")
+
 rescue StandardError => e
   puts e.inspect
   retry
 end
 
-# loop do
-#   puts 'Elige actividad a realizar (ruta/matriz)'
-#   option = ARGF.readline.strip
-#   next calculate_route(station_names:)   if option == 'ruta'
-#   next calculate_matrices if option == 'matriz'
+loop do
+  puts 'Elige actividad a realizar (ruta/matriz)'
+  option = ARGF.readline.strip
+  next calculate_route(station_names:)   if option == 'ruta'
+  next calculate_matrices if option == 'matriz'
   
 
-#   puts 'Continuar (si/no)'
-#   break if ARGF.readline.strip == 'no'
-# end
-ARGF.readline
-calculate_matrices
+  puts 'Continuar (si/no)'
+  break if ARGF.readline.strip == 'no'
+end
+# ARGF.readline
+# calculate_matrices
